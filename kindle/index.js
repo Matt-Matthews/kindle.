c0d4fded9f1a3866a60b5d19b93cbec4645b1a51
Matt-Matthews <65659429+Matt-Matthews@users.noreply.github.com>
2022-05-12 13:11:24 +0200

    Add files via upload

--- kindle/index.js
+++ kindle/index.js
@@ -1,4 +1,4 @@
-const login = document.getElementById('login');
+const login = document.getElementById('login');         //variables for the forms
 const register = document.getElementById('register');
 const signin = document.getElementById('Signin');
 const signup = document.getElementById('Signup');
@@ -6,7 +6,7 @@ const logo_ = document.getElementById('logo-container');
 signin.classList.add('active');
 logo_.style.bottom = '30%';
 
-function sigInTab(){
+function sigInTab(){            //function to open the login form tab
 
     login.style.opacity = '1';
     login.style.height = 'auto';
@@ -18,7 +18,7 @@ function sigInTab(){
     logo_.style.bottom = '30%';
 }
 
-function signUpTab(){
+function signUpTab(){               //function to open the register form tab
 
     login.style.opacity = '0';
     login.style.height = '0';
@@ -31,9 +31,9 @@ function signUpTab(){
     logo_.style.bottom = '0%';
 }
 
-signUpTab();
+signUpTab();                //open the register form tab on start
 
-function openTab(tab){
+function openTab(tab){          //onclick function to open the register or login form tab
     
     if(tab === 'Signin'){
         sigInTab();
@@ -41,15 +41,12 @@ function openTab(tab){
     else if(tab === 'Signup'){
         signUpTab();
     }
-}
-
-// the real data 
-
+} 
 
 //the user data
 
 
-const userName = document.getElementById('name');
+const userName = document.getElementById('name'); //variables for the input fields
 const email = document.getElementById('email');
 const surname = document.getElementById('surname');
 const passwrd = document.getElementById('password');
@@ -59,33 +56,33 @@ let user = [];
 const errMsg = document.getElementById('regErrMsg');
 
 
-document.querySelector('#img').addEventListener("change", function(){
+document.querySelector('#img').addEventListener("change", function(){       //listen the file input
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
-        sessionStorage.setItem('img', reader.result);
+        sessionStorage.setItem('img', reader.result);               //store the image file in a sessionStorage
     });
     reader.readAsDataURL(this.files[0]);
 });
 
-function createUser(){
+function createUser(){          //create user data and store in an array
 
-    if(userName.value !='' && surname.value!=''&& email.value!=''&& phoneNo.value!=''&& img.value!=''&& passwrd.value!='' &&cPasswrd!=''){
-        if(passwrd.value == cPasswrd.value){
+    if(userName.value !='' && surname.value!=''&& email.value!=''&& phoneNo.value!=''&& img.value!=''&& passwrd.value!='' &&cPasswrd!=''){  //check if the fields are not empty
+        if(passwrd.value == cPasswrd.value){        //checks if the new password is the same as the confirm password
 
-            user.push(userName.value, surname.value, email.value, phoneNo.value, passwrd.value);
+            user.push(userName.value, surname.value, email.value, phoneNo.value, passwrd.value);            //store the user data into an array
             errMsg.innerHTML = 'user created successfully';
             
-            sigInTab();
+            sigInTab();         //goes to the login form tab after storing the data 
         }
         else{
             errMsg.innerHTML = 'Password do not match!';
-            user.splice(0, user.length);
+            //user.splice(0, user.length);
         }
     }
     else{
         errMsg.innerHTML = 'The form is incomplete!'
-        user.splice(0, user.length);
+        //user.splice(0, user.length);
     }
 }
 
@@ -96,23 +93,28 @@ const userEmail = document.getElementById('userEmail');
 const welcomeName = document.getElementsByClassName('welcomeName');
 const ppic = document.getElementsByClassName('profileImg');
 
-function logUser(){
+function logUser(){                 //check the user data and compare it with the data from the array
 
-    if(userEmail.value != '' && userPasswd.value != ''){
-        if(userEmail.value ==user[2] && userPasswd.value == user[4]){
+    if(userEmail.value != '' && userPasswd.value != ''){            //check if the fields are not empty
+        if(userEmail.value ==user[2] && userPasswd.value == user[4]){   //validate the user data
         
-            sessionStorage.setItem('name', user[0]);
-            window.location.href = 'landingPage.html';
+            sessionStorage.setItem('name', user[0]);            //store the user data into a session storage
+            sessionStorage.setItem('surname', user[1]);
+            sessionStorage.setItem('email', user[2]);
+            sessionStorage.setItem('phoneNo', user[3]);
+            sessionStorage.setItem('passwrd', user[4]);
+
+            window.location.href = 'landingPage.html';          //navigate to the landing page
             
 
 
         } else {
-            logErrMsg.innerHTML = 'wrong email or password, or user does not exist!';
+            logErrMsg.innerHTML = 'wrong email or password, or user does not exist!';          //return this message if the user data does not match with the input
         }
 
     }
     else{
-        logErrMsg.innerHTML = 'Insert your email and password!';
+        logErrMsg.innerHTML = 'Insert your email and password!';                //return this message if the input fieldss are empty
     }
 }
 
--- kindle/landingPage.js
+++ kindle/landingPage.js
@@ -1,7 +1,7 @@
-document.getElementById('welcomeName').innerHTML = sessionStorage.getItem('name');
-        document.getElementById('profileImg').src = sessionStorage.getItem('img'); 
+document.getElementById('welcomeName').innerHTML = sessionStorage.getItem('name'); //display the name of the user on the landing page
+document.getElementById('profileImg').src = sessionStorage.getItem('img'); //display the image of the user on the landing page
         
-let authors = [{
+let authors = [{                //this array contains the list of famous authors
     author: 'James Patterson',
     numOfBooks: '16 Books',
     img: 'images/Authors/James-Patterson.jpg'
@@ -17,22 +17,30 @@ let authors = [{
     img: 'images/Authors/stephen-king.jpg'
 }];
 
-function renderAuthors(){
-    document.getElementById('authorImg1').src = authors[0].img;
-    document.getElementById('authorName1').innerHTML = authors[0].author;
-    document.getElementById('numOfBooks1').innerHTML = authors[0].numOfBooks;
-
-    document.getElementById('authorImg2').src = authors[1].img;
-    document.getElementById('authorName2').innerHTML = authors[1].author;
-    document.getElementById('numOfBooks2').innerHTML = authors[1].numOfBooks;
-
-    document.getElementById('authorImg3').src = authors[2].img;
-    document.getElementById('authorName3').innerHTML = authors[2].author;
-    document.getElementById('numOfBooks3').innerHTML = authors[2].numOfBooks;
+function renderAuthors(){               //this function render the list of famous authors
+    
+    authors.forEach((item, i) => {
+        let secondColumn = document.getElementById('second-column');
+        let authorRow = document.createElement('div');
+        authorRow.classList.add('author-row');
+        secondColumn.append(authorRow);
+        let img = document.createElement('img');
+        img.src = authors[i].img;
+        authorRow.append(img);
+        let authorDetails = document.createElement('div');
+        authorDetails.classList.add('author-details');
+        authorRow.append(authorDetails);
+        let authorName = document.createElement('h6');
+        authorName.innerHTML = authors[i].author;
+        authorDetails.append(authorName);
+        let numOfBooks = document.createElement('p');
+        numOfBooks.innerHTML = authors[i].numOfBooks;
+        authorDetails.append(numOfBooks);
+    });
 }
 renderAuthors();
 
-let newBooks = [
+let newBooks = [                    //this array contains of the list of new released books
     {
         bookImg: 'images/books/9780618640157_custom-s6-c30-682x1024.webp',
         bookName: 'The Lord of the Rings',
@@ -45,12 +53,7 @@ let newBooks = [
         author: 'Ray Bradbury',
         category: 'Mystery'
     },
-    {
-        bookImg: 'images/books/pride_and_prejudice_book_cover_by_fourblackbirds-d533108-723x1024.png',
-        bookName: 'Pride and Prejudice',
-        author: 'Jane Austen',
-        category: 'Mystery'
-    },
+    
     {
         bookImg: 'images/books/Penguin-2-668x1024.webp',
         bookName: 'The Great Gatsby',
@@ -63,40 +66,46 @@ let newBooks = [
         author: 'J.R.R Tolkien',
         category: 'Fantasy'
     },
+    {
+        bookImg: 'images/books/pride_and_prejudice_book_cover_by_fourblackbirds-d533108-723x1024.png',
+        bookName: 'Pride and Prejudice',
+        author: 'Jane Austen',
+        category: 'Mystery'
+    },
+    
+    
 ];
 
 
-function renderNewBooks()
+function renderNewBooks()               //this function renders the list of new released books
 {
 
-    document.getElementById('bookImg1').src = newBooks[0].bookImg;
-    document.getElementById('bookName1').innerHTML = newBooks[0].bookName;
-    document.getElementById('nameOfAuthor1').innerHTML = newBooks[0].author;
-    document.getElementById('Category1').innerHTML = newBooks[0].category;
+    newBooks.forEach((item, i) =>{
 
-    document.getElementById('bookImg2').src = newBooks[1].bookImg;
-    document.getElementById('bookName2').innerHTML = newBooks[1].bookName;
-    document.getElementById('nameOfAuthor2').innerHTML = newBooks[1].author;
-    document.getElementById('Category2').innerHTML = newBooks[1].category;
+        let bookList = document.getElementById('book-list');
+        let bookInfo = document.createElement('div');
+        bookInfo.classList.add('book-info');
+        bookList.append(bookInfo);
+        let bookImg = document.createElement('img');
+        bookImg.src = newBooks[i].bookImg;
+        bookInfo.append(bookImg);
+        let bookName = document.createElement('h3');
+        bookName.innerHTML = newBooks[i].bookName;
+        bookInfo.append(bookName);
+        let nameOfAuthor = document.createElement('p');
+        nameOfAuthor.innerHTML = newBooks[i].author;
+        bookInfo.append(nameOfAuthor);
+        let category = document.createElement('button');
+        category.innerHTML = newBooks[i].category;
+        bookInfo.append(category);
 
-    document.getElementById('bookImg3').src = newBooks[2].bookImg;
-    document.getElementById('bookName3').innerHTML = newBooks[2].bookName;
-    document.getElementById('nameOfAuthor3').innerHTML = newBooks[2].author;
-    document.getElementById('Category3').innerHTML = newBooks[2].category;
+    });
+    
 
-    document.getElementById('bookImg4').src = newBooks[3].bookImg;
-    document.getElementById('bookName4').innerHTML = newBooks[3].bookName;
-    document.getElementById('nameOfAuthor4').innerHTML = newBooks[3].author;
-    document.getElementById('Category4').innerHTML = newBooks[3].category;
-
-    document.getElementById('bookImg5').src = newBooks[4].bookImg;
-    document.getElementById('bookName5').innerHTML = newBooks[4].bookName;
-    document.getElementById('nameOfAuthor5').innerHTML = newBooks[4].author;
-    document.getElementById('Category5').innerHTML = newBooks[4].category;
 }
 renderNewBooks();
 
-let lastBooks = [
+let lastBooks = [               //this array contains the list of last read books
     {
         bookName: 'images/books/50-anniversary-cover1-350x564.webp',
         name: 'To kill a morkingbird',
@@ -121,38 +130,55 @@ let lastBooks = [
         bookName: 'images/books/diary-of-anne-frank-postcard-front_0-662x1024.jpg',
         name: 'Diary of anne frank',
         comment: '217 page - 11 chapter - last read',
-    }
+    },
+    
 ];
 
-function renderLastBooks(){
-    document.getElementById('lastBook1').src = lastBooks[0].bookName;
-    document.getElementById('lastBookName1').innerHTML = lastBooks[0].name;
-    document.getElementById('comment1').innerHTML = lastBooks[0].comment;
+function renderLastBooks(){                 //this function renders the last read books
+    
+    lastBooks.forEach((item,i)=>{
 
-    document.getElementById('lastBook2').src = lastBooks[1].bookName;
-    document.getElementById('lastBookName2').innerHTML = lastBooks[1].name;
-    document.getElementById('comment2').innerHTML = lastBooks[1].comment;
+        let list = document.getElementById('list');
+        let nestedList = document.createElement('li');
+        list.append(nestedList);
 
-    document.getElementById('lastBook3').src = lastBooks[2].bookName;
-    document.getElementById('lastBookName3').innerHTML = lastBooks[2].name;
-    document.getElementById('comment3').innerHTML = lastBooks[2].comment;
+        let imgCard = document.createElement('div');
+        imgCard.classList.add('imgCard');
+        imgCard.style.backgroundImage = 'url('+lastBooks[i].bookName+')';
+        nestedList.append(imgCard);
 
-    document.getElementById('lastBook4').src = lastBooks[3].bookName;
-    document.getElementById('lastBookName4').innerHTML = lastBooks[3].name;
-    document.getElementById('comment4').innerHTML = lastBooks[3].comment;
+        let card = document.createElement('div');
+        card.classList.add('card');
+        nestedList.append(card);
 
-    document.getElementById('lastBook5').src = lastBooks[4].bookName;
-    document.getElementById('lastBookName5').innerHTML = lastBooks[4].name;
-    document.getElementById('comment5').innerHTML = lastBooks[4].comment;
+        let bookRead = document.createElement('div');
+        bookRead.classList.add('book-read');
+        nestedList.append(bookRead);
+
+        let lastBookImg = document.createElement('img');
+        lastBookImg.src = lastBooks[i].bookName;
+        bookRead.append(lastBookImg);
+
+        let lastBookName = document.createElement('h3');
+        lastBookName.innerHTML = lastBooks[i].name;
+        bookRead.append(lastBookName);
+
+        let comment = document.createElement('p')
+        comment.innerHTML = lastBooks[i].comment;
+        bookRead.append(comment);
+
+    });
 
 }
+
 renderLastBooks();
 
 const list = document.querySelectorAll('#scrollable');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
-list.forEach((item, i) => {
+
+list.forEach((item, i) => {             //scrolls the list of last read books
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
 
@@ -167,5 +193,5 @@ list.forEach((item, i) => {
         prevBtn.style.display = 'none';
         nextBtn.style.display = 'block';
     })
+    
 });
-
